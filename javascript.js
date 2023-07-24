e57cb4b73f143a5544db785e699c120fcee2f8e7
8249596972 <subratmohanty2309@gmail.com>
2023-07-24 11:25:03 +0530

    add

--- javascript.js
+++ javascript.js
@@ -392,11 +392,11 @@ let n = 6;
 
 for(let i = 0 ; i < n ; i++){
   let row = ""
-  for(let j  = 0  ; j < i; j++){
+  for(let j  = 0  ; j < (n-i); j++){
     row += ""
   }
-  for(let k = 0  ; k < n-i ; k++){
-    row +="*"
+  for(let k = 0  ; k < (n-i) ; k++){
+    row +=" *"
 
   }
   console.log(row)
